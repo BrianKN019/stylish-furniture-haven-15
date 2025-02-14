b6e57283332b060828dd0a29390359e00a015f87
BRIAN KIMEMIA NJERI <briannkimemia@gmail.com>
2025-02-14 22:05:30 +0300

    Update OrderConfirmation.tsx

--- src/components/cart/OrderConfirmation.tsx
+++ src/components/cart/OrderConfirmation.tsx
@@ -1,4 +1,4 @@
-
+ 
 import { QRCodeSVG } from 'qrcode.react';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
